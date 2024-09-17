16701f47a3b9ff7836e69a4e8d487c1af49d510b
Keiler Trindade <keiler.trindade@hotmail.com>
2024-09-17 17:46:17 -0300

    add webscrapping pt 2

--- webscrapping/scraping.py
+++ webscrapping/scraping.py
@@ -1,26 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://zatchbell.fandom.com/wiki/Mamodos_and_Bookkeepers'
-response = requests.get(url)
+def get_mamodo_list_links():
+    url = 'https://zatchbell.fandom.com/wiki/Mamodos_and_Bookkeepers'
+    response = requests.get(url)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-#mamodo_soup = soup.find_all("li","toclevel-1")
-mamodo_soup = soup.select('#toc li a .toctext')
-
-mamodo_list = []
-mamodo_soup_list = []
-
-for mamodo in mamodo_soup:
-    #mamodo_list.append(mamodo)
-    mamodo_soup_list.append(mamodo.get_text())
-
-#print(mamodo_list)
-
-for mamodo in mamodo_soup_list:
-    mamodo = mamodo.replace(" ","_")
-    mamodo = 'https://zatchbell.fandom.com/wiki/'+mamodo
-    mamodo_list.append(mamodo)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    mamodo_soup = soup.select('#toc li a .toctext')
     
-with open('mamodo_list_final.txt', 'w', encoding="utf-8") as file:
-   file.write(repr(mamodo_list))
+    mamodo_soup_list = []
+    mamodo_list = []
+
+    for mamodo in mamodo_soup:
+        mamodo_soup_list.append(mamodo.get_text())
+
+    for mamodo in mamodo_soup_list:
+        mamodo = mamodo.replace(" ","_")
+        mamodo = 'https://zatchbell.fandom.com/wiki/'+mamodo
+        mamodo_list.append(mamodo)
+        
+    with open('mamodo_list_final.txt', 'w', encoding="utf-8") as file:
+        file.write("\n".join(mamodo_list) + "\n")
+  
+
+def get_mamodo_link_in_list():
+    mamodo_id = 0
+    with open('mamodo_list_final.txt', 'r', encoding="utf-8") as file:
+        #for mamodo in file:
+        mamodo = file.readline()
+        mamodo_id += 1
+        get_mamodo_informations(mamodo_id, mamodo.strip())
+
+
+def get_mamodo_informations(mamodo_id, mamodo):
+
+    #Remover parte do link para ter o Id do nome da dupla
+    id_dupla = mamodo.replace('https://zatchbell.fandom.com/wiki/',"")
+
+    response = requests.get(mamodo)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    dupla_nome = soup.select('#'+id_dupla )
+    spells = soup.select('Spells table')
+    print(dupla_nome)
+    print(spells)
+
+    with open('mamodo.html', 'w', encoding="utf-8") as file:
+        file.write(repr(soup))
+
+def get_spell_informations():
+    pass
+
+#get_mamodo_list_links()
+#get_mamodo_informations()
+
+get_mamodo_link_in_list()
